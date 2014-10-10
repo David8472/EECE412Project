a3ab2b29b5f060f2dbf486afb7407f9f1b7d80b1
Cyril Canonizado <cyril.canonizado@gmail.com>
2014-10-10 14:56:05 -0700

    implemented encryption function

--- VPN/src/encryption/RSA_encrypt.java
+++ VPN/src/encryption/RSA_encrypt.java
@@ -1,12 +1,12 @@
 package encryption;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 
 
 public class RSA_encrypt {
@@ -19,7 +19,24 @@ public class RSA_encrypt {
 		Key public_key = keys.getPublic();
 		Key private_key = keys.getPrivate();
 	
-		
+		byte[] ciphertext = encrypt("hello and goodbye", public_key);
+		System.out.println("cipher is " + ciphertext);
+	
 	}
+	
+	  public static byte[] encrypt(String plaintext, Key public_key) throws NoSuchAlgorithmException{
+		    byte[] cipherText = null;
+		   
+		    try{
+			    Cipher c = Cipher.getInstance("RSA");
+			    c.init(Cipher.ENCRYPT_MODE, public_key);
+			    cipherText = c.doFinal(plaintext.getBytes());
+		    }catch(Exception e){
+		    	
+		    }	    
+		    return cipherText;
+		  }
+	
+
 	
 }
